d1d2467c2a6400cd87e5a5656a8f7639a56c5f9b
Luann2003 <lvictorluciano@outlook.com>
2024-10-07 13:29:14 -0300

    about section

---
+++ js/animation.js
@@ -0,0 +1,9 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.content', {duration: 2000 })
+sr.reveal('.sociais', {duration: 2000 })
+sr.reveal('.container-projetos', { duration: 1000 });
+sr.reveal('.container-text', { duration: 1000 });
+sr.reveal('.container-image', { duration: 2000 });
+sr.reveal('.cards-projetos', { duration: 1000, interval: 200 }); 
+
