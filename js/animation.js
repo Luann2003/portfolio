6300ca960c031f0bed56fa9bce015d523e8de209
Luann2003 <lvictorluciano@outlook.com>
2024-10-08 09:58:41 -0300

    update

--- js/animation.js
+++ js/animation.js
@@ -4,6 +4,7 @@ sr.reveal('.content', {duration: 2000 })
 sr.reveal('.sociais', {duration: 2000 })
 sr.reveal('.container-projetos', { duration: 1000 });
 sr.reveal('.container-text', { duration: 1000 });
+sr.reveal('.skills-icons', { duration: 1000 });
 sr.reveal('.container-image', { duration: 2000 });
 sr.reveal('.cards-projetos', { duration: 1000, interval: 200 }); 
 
