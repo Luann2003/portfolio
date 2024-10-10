0a2283d4399eb7513a6115d24896db17e6145dcf
Luann2003 <lvictorluciano@outlook.com>
2024-10-10 13:10:05 -0300

    update button and details

--- js/modal.js
+++ js/modal.js
@@ -1,17 +1,22 @@
-const openModal = (title, info, image, date, linkedinUrl, githubUrl) => {
+const openModal = (title, info, image, date, linkedinUrl, githubUrl, tags) => {
     document.getElementById('modal').classList.add('active');
     document.getElementById('modal-title').innerText = title;
     document.getElementById("modal-info").innerText = info;
     document.getElementById("modal-image").src = image; 
     document.getElementById("modal-date").innerText = date; 
-    
-    
     document.getElementById('link-linkedin').href = linkedinUrl;
     document.getElementById('link-github').href = githubUrl;
-}
 
-const closeModal = () => {
-    document.getElementById('modal').classList.remove('active')
+    document.body.style.overflow = 'hidden';
+
+    const ul = document.querySelector('.modal-text ul');
+    ul.innerHTML = '';  // Limpa qualquer conteúdo anterior
+    tags.forEach(tag => {
+        const li = document.createElement('li');
+        li.innerHTML = `<span class="tag">${tag}</span>`;
+        ul.appendChild(li);
+    });
+
 }
 
 var cards = document.querySelectorAll('.cards-projetos');
@@ -21,11 +26,30 @@ cards.forEach(card => {
         const info = this.getAttribute('data-info'); 
         const image = this.getAttribute('data-image'); 
         const date = this.getAttribute('data-date');  
-        const linkedinUrl = this.getAttribute('data-linkedin'); // Extraindo o link do LinkedIn
-        const githubUrl = this.getAttribute('data-github'); // Extraindo o link do GitHub
+        const linkedinUrl = this.getAttribute('data-linkedin'); 
+        const githubUrl = this.getAttribute('data-github'); 
+        const tags = this.getAttribute('data-tags').split(',').map(tag => tag.trim()); 
         
-        openModal(title, info, image, date, linkedinUrl, githubUrl); // Passando os links;
+        openModal(title, info, image, date, linkedinUrl, githubUrl, tags);
     };
+});
+
+const closeModal = () => {
+    document.getElementById('modal').classList.remove('active')
+    document.body.style.overflow = 'auto';
+
+}
+
+document.getElementById('modal').addEventListener('click', function(event) {
+    if (event.target === this) {
+        closeModal();
+    }
+});
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === "Escape") { 
+        closeModal();
+    }
 });
 
 document.getElementById('modalClose')
